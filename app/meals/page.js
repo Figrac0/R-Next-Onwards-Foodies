295822a321a8b75baf8143de84de5584f5bc00c7
sergey <serjjiniuss@gmail.com>
2025-11-15 22:10:50 +0400

    firstC

---
+++ app/meals/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function MealPage() {
+    return (
+        <>
+            <h1>MealPage</h1>
+            <Link href="/meal/post1">Post1</Link>
+        </>
+    );
+}
+
+export default MealPage;
